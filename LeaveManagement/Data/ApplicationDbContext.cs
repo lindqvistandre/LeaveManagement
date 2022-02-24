9f249d755e2f7c329389de2f74d436ac755ecff8
André Lindqvist <71825454+lindqvistandre@users.noreply.github.com>
2022-02-24 16:13:35 +0100

    added database tables and migrations, end inoplement database section

--- LeaveManagement/Data/ApplicationDbContext.cs
+++ LeaveManagement/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LeaveManagement.Data.Migrations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagement.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Employee>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
     }
 }
---
+++ LeaveManagement/Data/LeaveAllocation.cs
@@ -0,0 +1,16 @@
+﻿using LeaveManagement.Data.Migrations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; } /*Fick inkludera data.migrations även om inte egentligen tänkt.*/
+        public int LeaveTypeId { get; set; }
+        public string EmployeeId { get; set; }
+
+    }
+}
