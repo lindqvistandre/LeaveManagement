77f393280c4ffef3d68c4098f4b74cc56f3d537a
André Lindqvist <71825454+lindqvistandre@users.noreply.github.com>
2022-03-03 14:58:31 +0100

    LeaveVM

---
+++ LeaveManagement/Configuration/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveManagement.Data.Migrations;
+using LeaveManagement.Models;
+
+namespace LeaveManagement.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
---
+++ LeaveManagement/Models/LeaveTypeVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        [Display(Name = "Leave Type Names")]
+        public string Name { get; set; }
+        [Display(Name = "Default Number Of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
